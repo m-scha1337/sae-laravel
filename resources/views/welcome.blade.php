78ba096be20617e88d7a85d7f2697e68ad70a291
g2 <g2@mxdt.at>
2020-08-10 22:26:21 +0200

    hello world

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title', 'Welcome')
+
+@section('container')
+
+    <div class="row">
+        <div class="col text-center">
+
+            <span class="fa fa-gamepad" style="font-size: 128px; margin: 40px 0;"></span>
+
+            <p class="lead">
+                Hello World<br><a href="https://github.com/passioncoder/sae-laravel" target="_blank">https://github.com/passioncoder/sae-laravel</a>
+            </p>
+
+        </div>
+    </div>
+
+@endsection
